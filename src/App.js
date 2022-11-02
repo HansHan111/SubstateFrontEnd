913b7b3d0db04d8ee13404962c4a2eb7f8bbb285
Hans Han <117279693+HansHan111@users.noreply.github.com>
2022-11-03 05:36:05 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
   Message,
 } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-
+ 
 import { SubstrateContextProvider, useSubstrateState } from './substrate-lib'
 import { DeveloperConsole } from './substrate-lib/components'
 
